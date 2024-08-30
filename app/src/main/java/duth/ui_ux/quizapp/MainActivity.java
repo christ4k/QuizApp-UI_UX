6c90396162fa4d88b723903c727f90d6bb2a3708
Chris Takaridis <chritaka@ee.duth.gr>
2024-08-30 14:17:05 +0300

    Change the way the answer is checked
    From now on the answer is checked by index and not as a string (by its
    literal content)

--- app/src/main/java/duth/ui_ux/quizapp/MainActivity.java
+++ app/src/main/java/duth/ui_ux/quizapp/MainActivity.java
@@ -4,25 +4,27 @@ import android.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.textclassifier.TextClassifier;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView questionTextView;
-    TextView totalQuestionsTextView;
+    TextView questionIndexTextView;
     Button ansA, ansB, ansC, ansD;
     Button btn_submit;
     int score = 0;
     int totalQuestions = QuestionAnswer.question.length;
     int currentQuestionIndex = 0;
-    String selectedAnswer="";
+    int selectedAnswer = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionIndexTextView = findViewById(R.id.question_index);
         questionTextView = findViewById(R.id.question);
         ansA = findViewById(R.id.ans_a);
         ansB = findViewById(R.id.ans_b);
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ansD.setOnClickListener(this);
         btn_submit.setOnClickListener(this);
 
-        totalQuestionsTextView.setText("Total questions: " + totalQuestions);
+        questionIndexTextView.setText("Question: " + currentQuestionIndex + " out of: " + totalQuestions);
         loadNewQuestion();
     }
 
@@ -52,17 +54,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             finishQuiz();
             return;
         }
-
         resetColors();
-
         questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
         ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
         ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
         ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
         ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
-
-        selectedAnswer = "";
-
+        selectedAnswer = -1;
     }
 
     private void finishQuiz(){
@@ -89,27 +87,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View view){
+    public void onClick(View v){
         Button clickedButton;
-        clickedButton = (Button) view;
-
+        clickedButton = (Button) v;
         if(clickedButton.getId() == R.id.btn_submit){
-            if(!selectedAnswer.isEmpty()){
-                if(selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])){
+            if(!(selectedAnswer == -1)){
+                if(selectedAnswer == (QuestionAnswer.correctAnswers[currentQuestionIndex])){
                     score++;
                 }
                 currentQuestionIndex++;
+                questionIndexTextView.setText("Question: " + currentQuestionIndex + " out of: " + totalQuestions);
                 loadNewQuestion();
-            }
-            else {
-                clickedButton.setBackgroundColor(Color.BLACK);
             }
         }
         else {
-            selectedAnswer = clickedButton.getText().toString();
             resetColors();
             clickedButton.setBackgroundColor(Color.parseColor("#81C9EF"));
             btn_submit.setBackgroundColor(Color.parseColor("#9D162C"));
+            if(clickedButton.getId() == R.id.ans_a){
+                selectedAnswer = 0;
+            }
+            else if(clickedButton.getId() == R.id.ans_b) {
+                selectedAnswer = 1;
+            }
+            else if(clickedButton.getId() == R.id.ans_c){
+                selectedAnswer = 2;
+            }
+            else if(clickedButton.getId() == R.id.ans_d){
+                selectedAnswer = 3;
+            }
+            else {
+                selectedAnswer = -1;
+            }
+            Log.d("selectedAnswer", Integer.toString(selectedAnswer));
         }
     }
 }
--- app/src/main/java/duth/ui_ux/quizapp/QuestionAnswer.java
+++ app/src/main/java/duth/ui_ux/quizapp/QuestionAnswer.java
@@ -2,23 +2,23 @@ package duth.ui_ux.quizapp;
 
 public class QuestionAnswer {
     public static String[] question = {
-            "How much does it cost ...",
-            "How invented ...",
-            "How is the creator of ...",
-            "Which OS will you use for ..."
+            "Η Βιωσιμότητα επιτυγχάνεται όταν παράγουμε κάτι ξοδεύοντας …",
+            "Για να αντιμετωπιστεί το πρόβλημα των Ζιζανίων",
+            "Διάλεξε το σωστό",
+            "Διάλεξε το σωστό"
     };
 
     public static String[][] choices = {
-            {"32", "42", "52", "62"},
+            {"Λίγα, με καλύτερο αποτέλεσμα για τον άνθρωπο και το περιβάλλον, τώρα και για το μέλλον",
+                    "Αρκετά, με αποτέλεσμα τη μέγιστη ποσότητα παραγωγής και την υπερκάλυψη των ανθρώπινων αναγκών",
+                    "Πολλά, με αποτέλεσμα την καλύτερη ποιότητα των προϊόντων και την αντοχή τους στο χρόνο",
+                    "Λίγα, με ικανοποιητική κάλυψη της τωρινών αναγκών του ανθρώπου"},
             {"Graham Bell", "Einstein", "Edison", "None of the above"},
             {"Bill Gates", "Elon Musk", "Jeff Bezos", "Steve Jobs"},
             {"Linux", "FreeBSD", "OpenBSD", "MacOS", "Windows"}
     };
 
-    public static String[] correctAnswers = {
-            "42",
-            "Einstein",
-            "Elon Musk",
-            "FreeBSD"
+    public static int[] correctAnswers = {
+            0,3,0,3
     };
 }
